79a6a6641b02657b451ccb23133ff9f7d2afce75
perissology <perissology@protonmail.com>
2017-09-22 11:31:42 -0700

    fix bug with generateClass import

--- js/vault.js
+++ js/vault.js
@@ -1,5 +1,5 @@
 const VaultAbi = require('../build/Vault.sol').VaultAbi;
 const VaultByteCode = require('../build/Vault.sol').VaultByteCode;
-const generateClass = require('eth-contract-class');
+const generateClass = require('eth-contract-class').default;
 
 module.exports = generateClass(VaultAbi, VaultByteCode);
