43517ca146cd0e19e2f34ef4fa80054af684f20b
perissology <perissology@protonmail.com>
2017-09-26 11:59:57 -0700

    add plugin to noteManager state

--- js/liquidPledging.js
+++ js/liquidPledging.js
@@ -80,6 +80,7 @@ module.exports = (test) => {
           manager.parentProject = res.parentProject;
           manager.canceled = res.canceled;
         }
+        manager.plugin = res.plugin;
         return manager;
       });
   };
