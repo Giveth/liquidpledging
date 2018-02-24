371fc582f8b1563238f20b9578424efda3323e8c
perissology <perissology@protonmail.com>
2018-02-24 10:01:14 -0800

    add LPFactory to index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const contracts = require('./build/contracts');
 exports.LiquidPledging = contracts.LiquidPledging;
 exports.LiquidPledgingState = require('./lib/liquidPledgingState.js');
 exports.LPVault = contracts.LPVault;
+exports.LPFactory = contracts.LPFactory;
